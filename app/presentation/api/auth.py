eaae0c95aae1522838c95af2c6defe05479f2be5
CatCod6r <ncebrinskij@gmail.com>
2026-04-27 18:15:40 +0300

    fix typo

--- app/presentation/api/auth.py
+++ app/presentation/api/auth.py
@@ -1,4 +1,4 @@
-kfrom fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from datetime import timedelta
