a93753803c8ceff6448b9ebfdc69941b388dbe4a
CatCod6r <ncebrinskij@gmail.com>
2026-04-27 17:19:11 +0300

    revert commit wtf was that

--- app/application/dtos/read_models.py
+++ app/application/dtos/read_models.py
@@ -1,16 +1,29 @@
-from pydantic import BaseModel
+from dataclasses import dataclass
 from typing import List, Optional
 from datetime import datetime
 
-class DrawingReadModel(BaseModel):
-    id: str
-    title: str
-    owner_id: str
-    created_at: datetime
-    # Зверніть увагу: ми повертаємо не список об'єктів користувачів, які поставили лайк, 
-    # а лише їхню кількість, оскільки для відображення стрічки цього зазвичай достатньо.
-    likes_count: int 
-
-class UserReadModel(BaseModel):
-    id: str
+@dataclass(frozen=True)
+class UserReadModel:
+    id: int
     email: str
+    nickname: str
+
+@dataclass(frozen=True)
+class LayerReadModel:
+    id: int
+    drawing_id: int
+    author_id: int
+    author_nickname: Optional[str]
+    image_data: str
+    created_at: datetime
+
+@dataclass(frozen=True)
+class DrawingReadModel:
+    id: int
+    owner_id: int
+    owner_email: Optional[str]
+    owner_nickname: Optional[str]
+    title: str
+    created_at: datetime
+    layers: List[LayerReadModel]
+    likes_count: int
